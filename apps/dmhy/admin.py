2cf069a7d7d6aaeb507edc557a07c8ca0fb624d1
498330580 <498330580@qq.com>
2019-04-28 23:25:59 +0800

    admin添加动漫花园显示

--- apps/dmhy/admin.py
+++ apps/dmhy/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import *
+
+
+class DmhyAllAdmin(admin.ModelAdmin):
+    list_display = ('name', 'time', 'zimuzu', 'fenlei', 'faburen', 'xiazai_url')
+
+
+admin.site.register(DmhyAll, DmhyAllAdmin)
