23a18b0ac949de774cf19b1103859afa4f24ea52
498330580 <498330580@qq.com>
2019-05-11 19:28:28 +0800

    xadmin后台优化

---
+++ apps/accounts/adminx.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019-05-11-0011 18:17
+# @Author  : Andy
+# @Email   : 498330580@qq.com
+# @File    : adminx.py
+# @Software: PyCharm
+
+
+# import xadmin
+# from .models import MyUser
+#
+#
+# class MyUserAdmin(object):
+#     list_display = ['username', 'email']
+#
+#
+# xadmin.site.register(MyUser, MyUserAdmin)
---
+++ apps/dmhy/adminx.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019-05-11-0011 18:19
+# @Author  : Andy
+# @Email   : 498330580@qq.com
+# @File    : adminx.py
+# @Software: PyCharm
+
+
+import xadmin
+from .models import DmhyAll
+
+
+class DmhyAllAdmin:
+    list_display = ('name', 'time', 'zimuzu', 'fenlei', 'faburen', 'xiazai_url',)
+
+
+xadmin.site.register(DmhyAll, DmhyAllAdmin)
